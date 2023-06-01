fa179d5c690785e3df5bbe192bd1960ec0195a9d
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2023-06-01 21:21:28 +0200

    Hack effect functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const audioControl = document.querySelector('[data-audio-control]');
 let muteAudio = true;
 
 const hackerAnimElem = document.querySelector('[data-hacker-anim]');
-// const characters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "#", "$", "%", "&", "?"];
+
 const characters = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "#", "$", "%", "&", "?"];
 
 const toggleAudio = () => {
@@ -20,37 +20,50 @@ const toggleAudio = () => {
 
 audioControl.addEventListener('click', toggleAudio);
 
+let interval = null;
+
 // const hackEffect = (e) => {
 //     let iterations = 0;
+//     clearInterval(interval);
 
-//     const interval = setInterval(() => {
+//     interval = setInterval(() => {
 //         e.target.innerText = e.target.innerText.split('')
-//         .map(letter => characters[Math.floor(Math.random() * 42)])
+//         .map((letter, index) => {
+//             if (index < iterations) {
+//                 return e.target.dataset.word[index];
+//             }
+//             return characters[Math.floor(Math.random() * 42)];
+//         })
 //         .join('');
 
-//         if (iterations >= 9) clearInterval(interval);
+//         if (iterations >= e.target.dataset.word.length) clearInterval(interval);
     
-//         iterations += 1;
-//     }, 50);    
+//         iterations += 1 / 6;
+//     }, 40);    
 // }
+
+// hackerAnimElem.addEventListener('mouseover', hackEffect);
 
 const hackEffect = (e) => {
     let iterations = 0;
+    clearInterval(interval);
 
-    const interval = setInterval(() => {
-        e.target.innerText = e.target.innerText.split('')
+    interval = setInterval(() => {
+        e.innerText = e.innerText.split('')
         .map((letter, index) => {
             if (index < iterations) {
-                return e.target.dataset.word[index];
+                return e.dataset.word[index];
             }
             return characters[Math.floor(Math.random() * 42)];
         })
         .join('');
 
-        if (iterations >= e.target.dataset.word.length) clearInterval(interval);
+        if (iterations >= e.dataset.word.length) clearInterval(interval);
     
         iterations += 1 / 6;
-    }, 50);    
+    }, 40);    
 }
 
-hackerAnimElem.addEventListener('mouseover', hackEffect);
+setInterval(() => {
+    hackEffect(hackerAnimElem);
+}, 10000);
